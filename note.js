613be8a8ddb60d431d904a18e21198f1618e26db
Hyssos <ondizol@gmail.com>
2023-04-14 09:56:25 +0200

    add some js

---
+++ note.js
@@ -0,0 +1,24 @@
+if (some expression that can be true or false) {
+  // The code for the "true" branch goes here
+  // That is, the code that we want to run when the above
+  // expression evaluates to true
+  // ...
+  // ...
+  // Potentially many lines of code
+  else if(some other expresseion) {
+  // The code for the "true" branch goes here
+  // That is, the code that we want to run when the above
+  // expression evaluates to true
+  // ...
+  // ...
+  // Potentially many lines of code
+}
+  else {
+// The code for the "false" branch goes here
+  // That is, the code that we want to run when the above
+  // expression evaluates to false
+  // ...
+  // ...
+  // Again, potentially many lines of code
+}
+}
